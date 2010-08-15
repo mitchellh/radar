f719ba4c761f4fd09024f4366710ca349139b276
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-08-14 23:04:25 -0700

    Stylistic change to align file paths for autoload

--- lib/radar.rb
+++ lib/radar.rb
@@ -2,8 +2,8 @@ require 'radar/version'
 require 'radar/error'
 
 module Radar
-  autoload :Application, 'radar/application'
-  autoload :Config, 'radar/config'
+  autoload :Application,    'radar/application'
+  autoload :Config,         'radar/config'
   autoload :ExceptionEvent, 'radar/exception_event'
-  autoload :Reporter, 'radar/reporter'
+  autoload :Reporter,       'radar/reporter'
 end
