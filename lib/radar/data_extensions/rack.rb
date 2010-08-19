4b3534677a7ae70159089f818168315a931935c3
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-08-19 08:37:46 -0700

    Properly merge in entire rack environment for Rack data extension, avoidin JSON circular references

--- lib/radar/data_extensions/rack.rb
+++ lib/radar/data_extensions/rack.rb
@@ -21,12 +21,22 @@ module Radar
           })
         end
 
-        Support::Hash.deep_merge!(result, :request => { :headers => extract_http_headers(@event.extra[:rack_env]) }) if @event.extra[:rack_env]
+        if @event.extra[:rack_env]
+          Support::Hash.deep_merge!(result, :request => { :headers => extract_http_headers(@event.extra[:rack_env]) })
+          Support::Hash.deep_merge!(result, :request => { :rack_env => extract_rack_env(@event.extra[:rack_env]) })
+        end
+
         result
       end
 
       protected
 
+      # Extracts only the HTTP headers from the rack environment,
+      # converting them to the proper HTTP format: `HTTP_CONTENT_TYPE`
+      # to `Content-Type`
+      #
+      # @param [Hash] env
+      # @return [Hash]
       def extract_http_headers(env)
         env.inject({}) do |acc, data|
           k, v = data
@@ -35,6 +45,22 @@ module Radar
             # Convert things like HTTP_CONTENT_TYPE to Content-Type (standard
             # HTTP header style)
             k = $1.to_s.split("_").map { |c| c.capitalize }.join("-")
+            acc[k] = v
+          end
+
+          acc
+        end
+      end
+
+      # Extracts the rack environment, ignoring HTTP headers and
+      # converting the values to strings if they're not an Array
+      # or Hash.
+      def extract_rack_env(env)
+        env.inject({}) do |acc, data|
+          k, v = data
+
+          if !(k =~ /^HTTP_/)
+            v = v.to_s if !v.is_a?(Array) && !v.is_a?(Hash) && !v.is_a?(Integer)
             acc[k] = v
           end
 
--- test/radar/data_extensions/rack_test.rb
+++ test/radar/data_extensions/rack_test.rb
@@ -23,5 +23,29 @@ class RackDataTest < Test::Unit::TestCase
       assert result[:request][:headers].has_key?("Content-Type")
       assert !result[:request][:headers].has_key?("other")
     end
+
+    context "merging in rack environment properly" do
+      setup do
+        @event.extra[:rack_env] = {
+          "HTTP_CONTENT_TYPE" => "text/html",
+          "other"             => 7,
+          "else"              => Class.new.new
+        }
+
+        @result = @instance.to_hash[:request][:rack_env]
+      end
+
+      should "not merge in the HTTP headers" do
+        assert !@result.has_key?("HTTP_CONTENT_TYPE")
+      end
+
+      should "merge in the numeric value as-is" do
+        assert_equal 7, @result["other"]
+      end
+
+      should "merge in the foreign class as a string" do
+        assert_equal @event.extra[:rack_env]["else"].to_s, @result["else"]
+      end
+    end
   end
 end
